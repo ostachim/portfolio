f0cc47e7d38a8514d37a26443839f2426ac3b080
Michal O <ostachim98@gmail.com>
2021-01-24 22:53:01 +0100

    delete default files, change App.js

---
+++ src/pages/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="portfolio">
+      <header>
+          <h2>Michał Ostachowski</h2>
+
+          <nav>
+            <ul>
+              <li>About</li>
+              <li>Technologie</li>
+              <li>Portfolio</li>
+              <li>Kontakt</li>
+            </ul>
+          </nav>
+
+      </header>
+      <div className="page">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
