934c81268e1ebc2b4839c6af68558326db1b75b9
Michal O <ostachim98@gmail.com>
2021-01-26 20:48:11 +0100

    change App.css App.js index.html

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import '../styles/App.css';
 
 function App() {
+  const leftBrace ="{"
+  const rightBrace = "}"
   return (
     <div className="portfolio">
       <header>
@@ -11,16 +13,62 @@ function App() {
           <nav>
             <ul>
               <li>About</li>
-              <li>Technologie</li>
               <li>Portfolio</li>
-              <li>Kontakt</li>
+              <li>Contact</li>
             </ul>
           </nav>
 
       </header>
-      <div className="page">
+
+      <div className="background">
+        <h2>Hi, My name is</h2>
+        <h1>Michał.</h1>
+
+        
+        <div className="code">  
+          <code>
+            <div className="codeWindow">
+
+            </div>
+            <div className="codeContainer">
+              <brace>{leftBrace}</brace><br/>
+              <p>"name"</p> : <p>"Michał"</p>,<br/>
+              <p>"surname"</p> : <p>"Ostachowski"</p>,<br/>
+              <p>"location"</p> : <p>"Cracow/Katowice"</p><br/>
+              <brace>{rightBrace}</brace>
+            </div>
+              
+          </code>
+          
+        </div>
         
       </div>
+      <main>
+
+        <div className="about">
+          <h5>About</h5>
+
+        </div>
+
+        <div className="technology">
+          
+        </div>
+
+        <div className="language">
+          
+        </div>
+
+        <div className="hobby">
+          
+        </div>
+        <div className="contact">
+          
+        </div>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
